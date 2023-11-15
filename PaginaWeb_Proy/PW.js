31b8c14ce14b1898f308ca699bea8978fa855da3
IsraelTorrez <141113013+IsraelTorrez@users.noreply.github.com>
2023-11-15 01:07:18 +0000

    Se creonuevas carpetas y archivos .js y .html

--- PaginaWeb_Proy/PW.js
+++ PaginaWeb_Proy/PW.js
@@ -6,13 +6,41 @@ function agregarAlCarrito() {
 }
 
 function validarRegistro() {
-    let nombre = document.getElementById('nombre').value;
-    let email = document.getElementById('email').value;
+    // Obtenemos los valores de los campos
+    var nombre = document.getElementById('nombre').value;
+    var email = document.getElementById('email').value;
 
-    if (nombre === '' || email === '') {
-        alert('Por favor, complete todos los campos del formulario.');
-        return false;
+    // Mostramos una confirmación antes de registrar
+    var confirmacion = confirm('¿Estás seguro de registrarte con los siguientes datos?\nNombre: ' + nombre + '\nEmail: ' + email);
+
+    // Si el usuario confirma, continuamos con el registro
+    if (confirmacion) {
+        alert('¡Registro exitoso!\nNombre: ' + nombre + '\nEmail: ' + email);
+        return true; // Esto enviará el formulario
+    } else {
+        alert('Registro cancelado.');
+        return false; // Esto evita que el formulario se envíe
     }
+}
+function buscar() {
+    var searchTerm = document.getElementById('search-input').value.toLowerCase();
+    var elements = document.querySelectorAll('.searchable');
+    var resultados = 0;
 
-    return true;
+    elements.forEach(function(element) {
+        var content = element.textContent.toLowerCase();
+
+        if (content.includes(searchTerm)) {
+            // Muestra el elemento que contiene la palabra buscada
+            element.style.display = 'block';
+            resultados++;
+        } else {
+            // Oculta los elementos que no coinciden con la búsqueda
+            element.style.display = 'none';
+        }
+    });
+
+    if (resultados === 0) {
+        alert('No se encontraron resultados para: ' + searchTerm);
+    }
 }
