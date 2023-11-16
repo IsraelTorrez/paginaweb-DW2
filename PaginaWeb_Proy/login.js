213c6dc24f1604cc1a9475aa84c34fe30d1b3a3d
IsraelTorrez <141113013+IsraelTorrez@users.noreply.github.com>
2023-11-16 22:56:14 +0000

    Se creo unaos archivos dentro de la carpeta PaginaWeb_Proy

---
+++ PaginaWeb_Proy/login.js
@@ -0,0 +1,21 @@
+function iniciarSesion() {
+    var email = document.getElementById('login-email').value;
+    var password = document.getElementById('login-password').value;
+
+    // Aquí deberías validar las credenciales contra un servidor o base de datos
+    // Por ahora, haré una verificación básica
+    if (email === 'isacmariaca@gmail.com' && password === '712310') {
+        localStorage.setItem('userEmail', email);
+        alert('Inicio de sesión exitoso!');
+        
+       
+      
+        
+        // Redirige a la página principal después de iniciar sesión
+        window.location.href = 'paginaHTML.html';
+        return false;
+    } else {
+        alert('Error en las credenciales. Inténtalo de nuevo.');
+    return false;
+    }
+}
